691231ceaee2283a0c0998fd5668d61e20a4ec8b
sk-py <77550660+sk-py@users.noreply.github.com>
2024-07-31 19:05:53 +0530

    Map Screen UI Completed - Started Home page

--- app/(modal)/mapScreen.tsx
+++ app/(modal)/mapScreen.tsx
@@ -162,16 +162,26 @@ import {
   TouchableOpacity,
   View,
   Alert,
+  Image,
+  Keyboard,
 } from "react-native";
-import React, { useEffect, useRef, useState } from "react";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
-import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import MapView, {
+  PROVIDER_GOOGLE,
+  Marker,
+  MapViewProps,
+} from "react-native-maps";
+import {
+  GooglePlaceDetail,
+  GooglePlacesAutocomplete,
+} from "react-native-google-places-autocomplete";
 import * as Location from "expo-location";
 import { Colors } from "@/constants/Colors";
 import { Ionicons } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import LottieView from "lottie-react-native";
+import { opacity } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
 
 type LocationType = {
   latitude: number;
@@ -191,12 +201,15 @@ const MapScreen = () => {
   });
   const [loading, setLoading] = useState(true);
   const [retryCount, setRetryCount] = useState(0);
+  const [LocationSearchValue, setLocationSearchValue] = useState("");
+  const [SelectedDetails, setSelectedDetails] =
+    useState<GooglePlaceDetail | null>(null);
   const maxRetry = 3;
 
   const [locationChanged, setLocationChanged] = useState(false);
 
   const animation = useRef<LottieView>(null);
-
+  const mapViewRef = useRef<MapView>(null);
   const saveLocation = async (loc: LocationType) => {
     try {
       await AsyncStorage.setItem("userLocation", JSON.stringify(loc));
@@ -263,6 +276,18 @@ const MapScreen = () => {
         setLocationChanged(true);
       }
 
+      if (mapViewRef.current) {
+        mapViewRef.current.animateToRegion(
+          {
+            latitude: newLocation.latitude,
+            longitude: newLocation.longitude,
+            latitudeDelta: newLocation.latDelta,
+            longitudeDelta: newLocation.longDelta,
+          },
+          1000
+        );
+      }
+
       setLoading(false);
     } catch (error) {
       if (retryCount < maxRetry) {
@@ -277,6 +302,32 @@ const MapScreen = () => {
       }
     }
   };
+
+  const LocationCrad = useCallback(() => {
+    return (
+      <View style={styles.locationCard}>
+        <Image
+          source={{ uri: SelectedDetails?.icon }}
+          style={{ height: 45, width: 45, resizeMode: "contain" }}
+        />
+        <View
+          style={{
+            width: "80%",
+            minHeight: 70,
+            alignItems: "flex-start",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ fontSize: 16, fontFamily: "LatoBold" }}>
+            {SelectedDetails?.name.slice(0, 40)}
+          </Text>
+          <Text style={{ color: Colors.mediumDark }}>
+            {SelectedDetails?.formatted_address.slice(0, 150)}
+          </Text>
+        </View>
+      </View>
+    );
+  }, [SelectedDetails]);
 
   useEffect(() => {
     const initializeLocation = async () => {
@@ -293,48 +344,10 @@ const MapScreen = () => {
     initializeLocation();
   }, [retryCount]);
 
+  // console.log(LocationSearchValue);
+
   return (
     <KeyboardAvoidingView behavior="height" style={styles.container}>
-      <GooglePlacesAutocomplete
-        keyboardShouldPersistTaps="always"
-        styles={{
-          container: {
-            flex: 0,
-            backgroundColor: "#FFFFFF",
-          },
-          textInputContainer: {
-            paddingTop: "1%",
-            paddingHorizontal: "2%",
-          },
-        }}
-        renderLeftButton={() => (
-          <View style={{ alignItems: "center", justifyContent: "center" }}>
-            <Ionicons
-              name="search"
-              size={20}
-              color={Colors.mediumDark}
-              style={{ paddingHorizontal: "2%" }}
-            />
-          </View>
-        )}
-        fetchDetails={true}
-        placeholder="Search"
-        onPress={(data, details = null) => {
-          const point = details?.geometry?.location;
-          if (!point) return;
-          const newLocation = {
-            ...location,
-            latitude: point.lat,
-            longitude: point.lng,
-          };
-          setLocation(newLocation);
-          saveLocation(newLocation);
-        }}
-        query={{
-          key: process.env.EXPO_PUBLIC_GOOGLE_API_KEY,
-          language: "en",
-        }}
-      />
       {loading ? (
         <View style={styles.loadingContainer}>
           <LottieView
@@ -355,9 +368,11 @@ const MapScreen = () => {
         </View>
       ) : (
         <MapView
-          showsMyLocationButton
+          ref={mapViewRef}
+          showsMyLocationButton={false}
           userLocationUpdateInterval={5000}
           provider={PROVIDER_GOOGLE}
+          rotateEnabled={false}
           region={{
             latitude: location.latitude,
             longitude: location.longitude,
@@ -369,7 +384,17 @@ const MapScreen = () => {
           showsBuildings
           showsCompass
           style={{ flex: 1 }}
-        />
+        >
+          {SelectedDetails !== null && (
+            <Marker
+              // draggable //* can use it later, as it requires reversegeocoding, emits an event that gives us cordinate object with latitude, longitude using that detail we need to fetch address and details
+              coordinate={{
+                latitude: SelectedDetails?.geometry?.location?.lat,
+                longitude: SelectedDetails?.geometry?.location?.lng,
+              }}
+            ></Marker>
+          )}
+        </MapView>
       )}
       {/* {true && (
         <TouchableOpacity
@@ -380,6 +405,75 @@ const MapScreen = () => {
           <Text style={styles.refreshBtnTxt}>REFRESH LOCATION</Text>
         </TouchableOpacity>
       )} */}
+      <GooglePlacesAutocomplete
+        // textInputProps={{
+        //   clearButtonMode: "while-editing",
+        //   value: LocationSearchValue,
+        //   onChangeText: setLocationSearchValue,
+        // }}
+        keepResultsAfterBlur={false}
+        nearbyPlacesAPI="GooglePlacesSearch"
+        debounce={100}
+        // predefinedPlaces={[{}]}     //* Will be helpful while already showing recently selected places or address in search suggestion
+        suppressDefaultStyles={true} //* Will be helpful later to create swiggy/zomato like floating search bar
+        keyboardShouldPersistTaps="always"
+        listEmptyComponent={
+          <View style={styles.listEmptyComp}>
+            <Text>
+              Sorry, we couldn't find a match for the place you were looking
+              for.
+            </Text>
+          </View>
+        }
+        styles={styles.floatingSearchBar}
+        renderLeftButton={() => (
+          <View style={{ alignItems: "center", justifyContent: "center" }}>
+            <Ionicons
+              name="search"
+              size={25}
+              color={"grey"}
+              style={{ paddingRight: "2%" }}
+            />
+          </View>
+        )}
+        fetchDetails={true}
+        placeholder="Search for area, street name..."
+        onPress={(data, details = null) => {
+          setSelectedDetails(details);
+          // setLocationSearchValue("");
+          const point = details?.geometry?.location;
+          if (!point) return;
+          const newLocation = {
+            ...location,
+            latitude: point.lat,
+            longitude: point.lng,
+          };
+          setLocation(newLocation);
+          saveLocation(newLocation);
+        }}
+        query={{
+          key: process.env.EXPO_PUBLIC_GOOGLE_API_KEY,
+          language: "en",
+        }}
+      />
+      <TouchableOpacity
+        onPress={fetchLocation}
+        activeOpacity={0.7}
+        style={styles.locateMe}
+      >
+        <Ionicons name="locate" color={Colors.primary} size={25} />
+        {/* <Text
+          style={{
+            color: Colors.primary,
+            fontFamily: "LatoBold",
+            textTransform: "uppercase",
+          }}
+        >
+          Locate me
+        </Text> */}
+      </TouchableOpacity>
+      {SelectedDetails !== null && <LocationCrad />}
+
       <TouchableOpacity style={styles.confirmBtn} activeOpacity={0.6}>
         <Text style={styles.confirmBtnTxt}>CONFIRM</Text>
       </TouchableOpacity>
@@ -432,5 +526,109 @@ const styles = StyleSheet.create({
     color: "#FFFFFF",
     fontSize: 17,
     fontFamily: "LatoBold",
+  },
+  locationCard: {
+    width: "95%",
+    marginHorizontal: "2.5%",
+    position: "absolute",
+    flexDirection: "row",
+    padding: "1%",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    gap: 10,
+    paddingHorizontal: "2%",
+    borderRadius: 10,
+    bottom: "10%",
+    shadowColor: "#000000b5",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 4,
+  },
+  floatingSearchBar: {
+    container: {
+      // flex: 0,
+      position: "absolute",
+      zIndex: 1,
+      width: "90%",
+      top: "2%",
+      marginHorizontal: "5%",
+      padding: "2%",
+      backgroundColor: "#FFF",
+      borderRadius: 7,
+      borderColor: Colors.ripple,
+      // borderWidth: 0.4,
+      shadowColor: "#ff6600b5",
+      shadowOffset: { width: 0, height: 1 },
+      shadowOpacity: 0.8,
+      shadowRadius: 2,
+      elevation: 4,
+      alignItems: "flex-start",
+      justifyContent: "center",
+      overflow: "hidden",
+    },
+    textInputContainer: {
+      flexDirection: "row",
+      paddingHorizontal: "2%",
+      innerWidth: "100%",
+    },
+    textInput: {
+      marginLeft: "3%",
+      fontFamily: "LatoMed",
+      color: Colors.mediumDark,
+      width: "100%",
+      cursorColor: Colors.primary,
+    },
+    listView: {
+      backgroundColor: "#dadada17",
+      paddingTop: "4%",
+      padding: "2%",
+    },
+    row: {
+      padding: "2%",
+      alignItems: "flex-start",
+    },
+    description: {
+      color: Colors.mediumDark,
+    },
+    poweredContainer: {
+      width: "100%",
+      alignItems: "flex-end",
+      paddingTop: "2%",
+    },
+    separator: {
+      height: 0.7,
+      backgroundColor: Colors.grey,
+      opacity: 0.7,
+    },
+  },
+  listEmptyComp: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: "5%",
+    backgroundColor: "#FFF",
+    borderBottomWidth: 0.2,
+    borderBottomColor: "lightgrey",
+  },
+  locateMe: {
+    position: "absolute",
+    zIndex: 0,
+    top: "10%",
+    right: "4%",
+    paddingHorizontal: "2%",
+    // marginHorizontal: "30%",
+    // width: "40%",
+    borderRadius: 8,
+    padding: "1.5%",
+    gap: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#FFF",
+    shadowColor: "#000000b5",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 4,
+    justifyContent: "center",
   },
 });
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,36 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, Dimensions } from "react-native";
+import React, { useRef } from "react";
+import LottieView from "lottie-react-native";
+import { Colors } from "@/constants/Colors";
 
 const index = () => {
+  const animation = useRef<LottieView>(null);
+
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <ScrollView>
+      <View style={{ backgroundColor: "#FFFFFF", alignItems: "center" }}>
+        <Text
+          style={{
+            fontFamily: "LatoBold",
+            fontSize: 17,
+            color: Colors.mediumDark,
+          }}
+        >
+          Find Every Offer, from Good to Best!
+        </Text>
+      </View>
+      <LottieView
+        autoPlay
+        ref={animation}
+        style={{
+          // width: "15%",
+          // height: "15%",
+          width: "100%",
+          height: 100,
+        }}
+        source={require("../assets/Offers.json")}
+      />
+    </ScrollView>
   );
 };
 
