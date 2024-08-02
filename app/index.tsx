2fff1fd764e7d2e4a14fc15e41395922306f8f23
sk-py <77550660+sk-py@users.noreply.github.com>
2024-08-02 19:33:01 +0530

    Restauramts cards UI completed

--- app/index.tsx
+++ app/index.tsx
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   FlatList,
   Image,
+  TouchableOpacity,
 } from "react-native";
 import React from "react";
 import { Colors } from "@/constants/Colors";
@@ -13,6 +14,8 @@ import ParallaxCarousel from "@/components/ParallaxCarousel";
 import MiniCarousel from "@/components/MiniCarousel";
 import Title from "@/components/Title";
 import StrikeText from "@/components/StrikeText";
+
+import { LinearGradient } from "expo-linear-gradient";
 
 const HorizontalSliderImages = [
   {
@@ -44,72 +47,96 @@ const restaurantsData = [
     name: "Mc Donalds",
     distance: "1 Km",
     img: require("@/assets/images/macD.png"),
+    offer: ["60% OFF", "UPTO ₹120"],
+    desc: "Burgers, Fast Food, Snacks",
   },
   {
     id: 2,
     name: "Chings",
     distance: "1 Km",
     img: require("@/assets/images/kungPaoChicken.jpg"),
+    offer: ["30% OFF", "UPTO ₹60"],
+    desc: "Chinese, Noodles, Dumplings",
   },
   {
     id: 3,
     name: "Pawan Restaurant",
     distance: "1 Km",
     img: require("@/assets/images/northDish.jpg"),
+    offer: ["FLAT ₹50", "OFF"],
+    desc: "North Indian, Curries, Roti, Paneer Dishes",
   },
   {
     id: 4,
     name: "Wow Momos",
     distance: "1 Km",
     img: require("@/assets/images/momos.jpg"),
+    offer: ["FLAT 40% OFF"],
+    desc: "Momos, Dumplings, Fast Food",
   },
   {
     id: 5,
-    name: "Tandoori Deight",
+    name: "Tandoori Delight",
     distance: "1 Km",
     img: require("@/assets/images/tandoori.jpg"),
+    offer: ["FLAT ₹80 OFF"],
+    desc: "Tandoori Chicken, Kebabs, Naan",
   },
   {
     id: 6,
     name: "Bombay Duck",
     distance: "1 Km",
     img: require("@/assets/images/prawns.jpeg"),
+    offer: ["30% OFF", "UPTO ₹75"],
+    desc: "Seafood, Prawns, Fish Curry",
   },
   {
     id: 7,
     name: "Pizza Hut",
     distance: "1 Km",
     img: require("@/assets/images/pizzaRest.jpg"),
+    offer: ["Buy 1 Get 1"],
+    desc: "Pizza, Pasta, Garlic Bread",
   },
   {
     id: 8,
-    name: "Culinary Deight",
+    name: "Culinary Delight",
     distance: "1 Km",
     img: require("@/assets/images/culinaryDelight.png"),
+    offer: ["FLAT ₹125 OFF", "Above ₹399"],
+    desc: "Gourmet Dishes, Continental, Fusion Cuisine",
   },
   {
     id: 9,
     name: "Gourmet Cafe",
     distance: "1 Km",
     img: require("@/assets/images/cheesyBrocolli.jpg"),
+    offer: ["FLAT ₹60 OFF", "Above ₹299"],
+    desc: "Cafe, Sandwiches, Salads, Coffee",
   },
   {
     id: 10,
     name: "Momos Hub",
     distance: "1 Km",
     img: require("@/assets/images/schezwanMomos.jpg"),
+    offer: ["ITEMS", "AT ₹50"],
+    desc: "Momos, Fast Food, Dumplings",
   },
   {
     id: 11,
     name: "Udupi House",
     distance: "1 Km",
     img: require("@/assets/images/veg1.jpg"),
+    offer: ["50% OFF", "UPTO ₹199"],
+    desc: "South Indian, Dosa, Idli, Vada",
   },
   {
     id: 12,
-    name: "Bombay Duck",
+    name: "Dominoes",
     distance: "1 Km",
     img: require("@/assets/images/Pizza.jpg"),
+    offer: ["FLAT 30% OFF"],
+    desc: "Pizza, Pasta, Chicken Wings",
   },
 ];
 
@@ -117,7 +144,12 @@ const index = () => {
   return (
     <ScrollView
       showsVerticalScrollIndicator={false}
-      style={{ flex: 1, backgroundColor: "#FFFFFF" }}
+      style={{
+        flex: 1,
+        backgroundColor: "#FFF",
+        position: "relative",
+        zIndex: 5,
+      }}
     >
       <Title
         align="center"
@@ -138,24 +170,11 @@ const index = () => {
         opacity={0.6}
         strikes={3}
       />
-      {/* <FlatList
-        horizontal
-        data={restaurantsData}
-        renderItem={renderRestaurantsCard}
-        contentContainerStyle={{
-          gap: 10,
-          paddingVertical: "4%",
-          paddingHorizontal: "2%",
-          width: Dimensions.get("window").width * 2,
-          flexWrap: "wrap",
-        }}
-        showsHorizontalScrollIndicator={false}
-      /> */}
       <ScrollView
         horizontal
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={{
-          width: 1100,
+          width: 1110,
           flexWrap: "wrap",
           gap: 20,
           paddingVertical: 30,
@@ -166,16 +185,104 @@ const index = () => {
       >
         {restaurantsData.map((item) => {
           return (
-            <View key={item.id} style={{ width: 160, height: 200 }}>
-              <Image
-                source={item.img}
+            <TouchableOpacity key={item?.id} activeOpacity={0.7}>
+              <View
                 style={{
-                  height: "100%",
-                  width: "100%",
-                  resizeMode: "cover",
+                  width: 160,
+                  height: 200,
+                  position: "relative",
+                  borderRadius: 10,
+                  overflow: "hidden",
+                  backgroundColor: "#FFF",
+                  shadowColor: "#000000cc",
+                  shadowOffset: { width: 0, height: 5 },
+                  shadowOpacity: 0.8,
+                  shadowRadius: 2,
+                  elevation: 6,
                 }}
-              />
-            </View>
+              >
+                <Image
+                  source={item.img}
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                    resizeMode: "cover",
+                  }}
+                />
+                <View
+                  style={{
+                    position: "absolute",
+                    width: 160,
+                    height: 200,
+                    flexDirection: "column",
+                    alignItems: "flex-start",
+                    justifyContent: "flex-end",
+                    zIndex: 2,
+                  }}
+                >
+                  <View
+                    style={{
+                      paddingHorizontal: 10,
+                      paddingVertical: 8,
+                      gap: 4,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "white",
+                        fontFamily: "LatoBold",
+                        fontSize: 16,
+                      }}
+                    >
+                      {item.offer ? item.offer[0].toString() : ""}
+                    </Text>
+                    {item.offer && item.offer.length > 1 && (
+                      <Text
+                        style={{
+                          color: "white",
+                          fontFamily: "LatoMed",
+                          fontSize: 13,
+                        }}
+                      >
+                        {item.offer[1].toString()}
+                      </Text>
+                    )}
+                  </View>
+                </View>
+                <LinearGradient
+                  // Background Linear Gradient
+                  colors={["transparent", "rgba(0, 0, 0, 0.801)"]}
+                  style={{
+                    position: "absolute",
+                    left: 0,
+                    right: 0,
+                    top: 0,
+                    width: 160,
+                    height: 200,
+                  }}
+                />
+              </View>
+              <View style={{ maxWidth: 160, overflow: "hidden" }}>
+                <View style={{ padding: 5, flexDirection: "column" }}>
+                  <Text style={{ fontFamily: "LatoBold", fontSize: 18 }}>
+                    {item?.name.length > 16
+                      ? `${item?.name.slice(0, 14)}...`
+                      : item?.name}
+                  </Text>
+                  <Text
+                    style={{
+                      fontFamily: "LatoMed",
+                      fontSize: 13,
+                      color: Colors.medium,
+                    }}
+                  >
+                    {item?.desc.length > 25
+                      ? `${item?.desc.slice(0, 25)}...`
+                      : item?.desc}
+                  </Text>
+                </View>
+              </View>
+            </TouchableOpacity>
           );
         })}
       </ScrollView>
--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -5,41 +5,45 @@ import {
   TouchableOpacity,
   Image,
   TextInput,
+  Dimensions,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
 import { Colors } from "@/constants/Colors";
 import { Link } from "expo-router";
 import BottomSheetComp from "./BottomSheetComp";
+import LottieView from "lottie-react-native";
 
-const SearchBar = () => (
-  <View style={styles.searchContainer}>
-    <View style={styles.searchSection}>
-      <View style={styles.searchField}>
-        <Ionicons name="search" color={Colors.medium} size={22} />
-        <TextInput
-          enterKeyHint="search"
-          style={{
-            padding: 8,
-            color: Colors.mediumDark,
-            width: "100%",
-            fontFamily: "LatoMed",
-          }}
-          inputMode="text"
-          cursorColor={"orange"}
-          placeholder="Restaurants and dishes"
-          placeholderTextColor={Colors.medium}
-        />
+const SearchBar = () => {
+  return (
+    <View style={styles.searchContainer}>
+      <View style={styles.searchSection}>
+        <View style={styles.searchField}>
+          <Ionicons name="search" color={Colors.medium} size={22} />
+          <TextInput
+            enterKeyHint="search"
+            style={{
+              padding: 8,
+              color: Colors.mediumDark,
+              width: "100%",
+              fontFamily: "LatoMed",
+            }}
+            inputMode="text"
+            cursorColor={"orange"}
+            placeholder="Restaurants and dishes"
+            placeholderTextColor={Colors.medium}
+          />
+        </View>
+        <Link href={"/(modal)/filter"} asChild>
+          <TouchableOpacity style={styles.optionBtn}>
+            <Ionicons name="options-outline" size={25} color={Colors.primary} />
+          </TouchableOpacity>
+        </Link>
       </View>
-      <Link href={"/(modal)/filter"} asChild>
-        <TouchableOpacity style={styles.optionBtn}>
-          <Ionicons name="options-outline" size={25} color={Colors.primary} />
-        </TouchableOpacity>
-      </Link>
     </View>
-  </View>
-);
+  );
+};
 
 const CustomHeader = () => {
   const [ShowBottomSheet, setShowBottomSheet] = useState({
@@ -47,9 +51,46 @@ const CustomHeader = () => {
     calledBy: "",
   });
 
+  const [temperature, settemperature] = useState("Sun");
+
+  const getWetherDetails = async () => {
+    try {
+      const data = await fetch(
+        "http://api.weatherapi.com/v1/current.json?key=a949930bca0d46fa92f115049240208&q=Mumbai"
+      );
+      const temp = await data.json();
+      const { temp_c, condition, precip_mm } = temp?.current;
+      if (condition?.text?.toLowerCase()?.includes("rain") || precip_mm > 0) {
+        settemperature("Rain");
+      } else if (
+        temp_c < 19 ||
+        condition?.text?.toLowerCase()?.includes("snow")
+      ) {
+        settemperature("Winter");
+      } else if (
+        temp_c < 28 ||
+        condition?.text?.toLowerCase()?.includes("sun")
+      ) {
+        settemperature("Sun");
+      } else {
+        settemperature("Sun");
+      }
+
+      console.log(temp?.current?.temp_c);
+    } catch (error) {
+      console.log("Error while fetching data: ", error);
+    }
+  };
+
+  useEffect(() => {
+    getWetherDetails();
+  }, []);
+
   const openBottomSheet = () => {
     setShowBottomSheet({ state: !ShowBottomSheet.state, calledBy: "header" });
   };
+
+  const animation = useRef<LottieView>(null);
 
   return (
     <>
@@ -58,7 +99,39 @@ const CustomHeader = () => {
           ShowBottomSheet={ShowBottomSheet}
           setShowBottomSheet={setShowBottomSheet}
         />
+        <LottieView
+          autoPlay
+          ref={animation}
+          style={{
+            width: Dimensions.get("window").width,
+            height: 5,
+            position: "absolute",
+            zIndex: -5,
+          }}
+          direction={1}
+          loop
+          resizeMode="cover"
+          source={
+            temperature == "Winter"
+              ? require("@/assets/images/Winter.json")
+              : temperature == "Rain"
+              ? require("../assets/images/Rain.json")
+              : require("@/assets/images/Sun.json")
+          }
+        />
         <View style={styles.container}>
+          {/* <LinearGradient
+            colors={["#e7b9ad84", "transparent"]}
+            style={{
+              position: "absolute",
+              left: 0,
+              right: 0,
+              top: 0,
+              width: Dimensions.get("window").width,
+              height: "100%",
+              zIndex: -1,
+            }}
+          /> */}
           <TouchableOpacity>
             <Image
               source={require("@/assets/images/deliveryboy.jpg")}
@@ -66,11 +139,15 @@ const CustomHeader = () => {
               style={styles.bikerImg}
             />
           </TouchableOpacity>
-          <View style={styles.headerTitleBtn}>
+          <TouchableOpacity
+            onPress={openBottomSheet}
+            style={styles.headerTitleBtn}
+          >
             <Text
               style={{
                 fontSize: 14,
-                color: Colors.medium,
+                color: "grey",
+                zIndex: 5,
                 fontFamily: "LatoBold",
               }}
             >
@@ -95,7 +172,7 @@ const CustomHeader = () => {
                 color={Colors.primary}
               />
             </TouchableOpacity>
-          </View>
+          </TouchableOpacity>
           <TouchableOpacity style={styles.profileBtn}>
             <Ionicons name="person" size={20} color={Colors.primary} />
           </TouchableOpacity>
@@ -108,28 +185,33 @@ const CustomHeader = () => {
 
 const styles = StyleSheet.create({
   safeAreaStyles: {
-    backgroundColor: "#FFF",
+    // backgroundColor: "transparent",
+    position: "relative",
   },
   container: {
+    position: "relative",
     height: 60,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
     gap: 5,
     paddingHorizontal: "2%",
-    backgroundColor: "#FFF",
-
+    backgroundColor: "transparent",
+    zIndex: 0,
     // backgroundColor: "red",
   },
   bikerImg: {
     height: 40,
     width: 40,
     borderRadius: 50,
+    zIndex: 5,
   },
   headerTitleBtn: {
     flex: 1,
     alignItems: "flex-start",
     justifyContent: "center",
+    position: "relative",
+    zIndex: 10,
   },
   profileBtn: {
     backgroundColor: Colors.lightGrey,
@@ -139,6 +221,8 @@ const styles = StyleSheet.create({
   searchContainer: {
     height: 60,
     backgroundColor: "#FFF",
+    position: "relative",
+    zIndex: 5,
   },
   searchSection: {
     flexDirection: "row",
